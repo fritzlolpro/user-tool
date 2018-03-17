568455d9e5d2654ed4ddf3c2eef1e46a0329cb58
Fedor <livsant123@gmail.com>
2018-03-17 21:07:34 +0300

    tests
    app, UAF, UIE

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({adapter: new Adapter()})
+
+const wrapper = shallow(<App/>);
+
+describe('App', () => {
+    it('renders without exploding', () => {
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('should be defined', () => {
+        expect(wrapper).toBeDefined();
+    });
+
+    test('has a valid snapshot', () => {
+        const component = shallow(<App/>)
+
+        expect(component).toMatchSnapshot();
+
+    });
+
+})
--- src/Forms/UserInfoEditor.test.js
+++ src/Forms/UserInfoEditor.test.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import UserAddForm from './UserAddForm';
+import UserInfoEditor from './UserInfoEditor';
 import Enzyme, {shallow} from 'enzyme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()})
 
-describe('UserAddForm', () => {
-
-    const props = {
-        disablePostButton: () => {
-            return true
-        }
-    }
+describe('UserInfoEditor', () => {
 
     it('should be defined', () => {
-        expect(UserAddForm).toBeDefined();
+        expect(UserInfoEditor).toBeDefined();
     });
 
     it('renders without crashing'),
@@ -25,14 +19,14 @@ describe('UserAddForm', () => {
         const div = document.createElement('div');
         ReactDOM.render(
             <MuiThemeProvider>
-            <UserAddForm {...props}/>
+            <UserInfoEditor/>
         </MuiThemeProvider>, div);
     }
 
     test('has a valid snapshot', () => {
         const component = shallow(
             <MuiThemeProvider>
-                <UserAddForm {...props}/>
+                <UserInfoEditor/>
             </MuiThemeProvider>
         )
 
